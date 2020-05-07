4873616ffb3bbc1a53ba5c382b7d3a4259a4c8e5
Harish Bakka <harish@SKIMS027.local>
2020-05-07 10:02:28 -0700

    Added additional logics such as if else and case

--- test.rb
+++ test.rb
@@ -45,4 +45,60 @@ hsh.each do |key, value|
   print key, " is ", value, "\n"
 end
 
+#Ruby IF...ELSE Conditions
+# Syntax
+=begin
+if conditional [then]
+code...
+    [elsif conditional [then]
+code...]...
+    [else
+       code...]
+end
+=end
+#Sample Block
+puts "+++++..IF......ELSE....ELSIF..++++"
+x = 1
+if x > 2
+  puts "x is greater than 2"
+elsif x <= 2 and x!=0
+  puts "x is 1"
+else
+  puts "I can't guess the number"
+end
 
+$debug = 1
+print "debug\n" if $debug
+
+#Unless Statement
+#Executes code if conditional is false. If the conditional is true, code specified in the else clause is executed.
+
+x = 1
+unless x>=2
+  puts "x is less than 2"
+else
+  puts "x is greater than 2"
+end
+
+#CASE expressions
+$age =  5
+case $age
+when 0 .. 2
+  puts "baby"
+when 3 .. 6
+  puts "little child"
+when 7 .. 12
+  puts "child"
+when 13 .. 18
+  puts "youth"
+else
+  puts "adult"
+end
+
+# LOOPS
+$i = 0
+$num = 5
+while $i < $num  do
+  puts("Inside the loop i = #$i" )
+  $i +=1
+end
